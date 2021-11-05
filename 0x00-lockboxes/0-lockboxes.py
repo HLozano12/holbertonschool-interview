d75cfa3981e5c6837faa53111fd36fc94ec694d4
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-11-05 12:14:44 -0500

    range

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,20 +6,15 @@ def canUnlockAll(boxes):
 	"""
 	Write a method that determines if all the boxes can be opened
 	"""
-	key = [0]
-	lock = 0
+	lock = [0]
+    	viewed = 0
 
-	while key < len(lock):
-		key = len(lock)
-		for box in range(len(boxes)):
-			if box in lock:
-				continue
-			for item in boxes[box]:
-				if item in lock:
-					lock.append(box)
-					break
-			else:
-				continue
-			break
-		else:
-			return False
+    	while viewed < len(lock):
+        	viewed = len(lock)
+        for box in range(len(boxes)):
+            if box in lock:
+                for key in boxes[box]:
+                    if key < len(boxes) and key not in lock:
+                        lock.append(key)
+
+    	return len(lock) == len(boxes)
