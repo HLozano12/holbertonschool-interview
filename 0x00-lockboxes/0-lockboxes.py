63dd52eb116e775f42058f122e0908987834cec6
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-11-05 12:07:12 -0500

    true?

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,47 +6,20 @@ def canUnlockAll(boxes):
 	"""
 	Write a method that determines if all the boxes can be opened
 	"""
-	if not boxes:
-		return False
-	if len(boxes) == 1:
-		return True
-	if len(boxes) == 2:
-		if boxes[0] == 0 or boxes[1] == 0:
-			return True
-		else:
-			return False
-	if len(boxes) == 3:
-		if boxes[0] == 0 or boxes[1] == 0 or boxes[2] == 0:
-			return True
-		else:
-			return False
-	if len(boxes) == 4:
-		if boxes[0] == 0 or boxes[1] == 0 or boxes[2] == 0 or boxes[3] == 0:
-			return True
-		else:
-			return False
-	if len(boxes) == 5:
-		if boxes[0] == 0 or boxes[1] == 0 or boxes[2] == 0 or boxes[3] == 0 or boxes[4] == 0:
-			return True
-		else:
-			return False
-	if len(boxes) == 6:
-		if boxes[0] == 0 or boxes[1] == 0 or boxes[2] == 0 or boxes[3] == 0 or boxes[4] == 0 or boxes[5] == 0:
-			return True
-		else:
-			return False
-	if len(boxes) == 7:
-		if boxes[0] == 0 or boxes[1] == 0 or boxes[2] == 0 or boxes[3] == 0 or boxes[4] == 0 or boxes[5] == 0 or boxes[6] == 0:
-			return True
-		else:
-			return False
-	if len(boxes) == 8:
-		if boxes[0] == 0 or boxes[1] == 0 or boxes[2] == 0 or boxes[3] == 0 or boxes[4] == 0 or boxes[5] == 0 or boxes[6] == 0 or boxes[7] == 0:
-			return True
-		else:
-			return False
-	if len(boxes) == 9:
-		if boxes[0] == 0 or boxes[1] == 0 or boxes[2] == 0 or boxes[3] == 0 or boxes[4] == 0 or boxes[5] == 0 or boxes[6] == 0 or boxes[7] == 0 or boxes[8] == 0:
-			return True
+	key = [0]
+	lock = 0
+
+	while key < len(lock):
+		key = len(lock)
+		for box in range(len(boxes)):
+			if box in lock:
+				continue
+			for item in boxes[box]:
+				if item in lock:
+					lock.append(box)
+					break
+			else:
+				continue
+			break
 		else:
 			return False
