d28b76d64b639378ef1fa357e6ca73d56b3b5208
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-11-05 12:33:06 -0500

    comments

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,15 +3,16 @@
 
 
 def canUnlockAll(boxes):
-	
-	KeysView =[0]
-	prevBox = 0
-
-	while prevBox < len(KeysView):
-		prevBox = len(KeysView)
-		for box in range(len(boxes)):
-			if box in KeysView:
-				for key in boxes[box]:
-					if key < len(boxes) and key not in KeysView:
-						KeysView.append(key)
-	return len(KeysView) == len(boxes)
+    # Initialize a list of keys
+    KeysView = [0]
+    prevBox = 0
+    # Loop through the list of boxes
+    while prevBox < len(KeysView):
+        prevBox = len(KeysView)
+        for box in range(len(boxes)):
+            if box in KeysView:
+                for key in boxes[box]:
+                    if key < len(boxes) and key not in KeysView:
+                        KeysView.append(key)
+    # Return True if all the boxes can be opened
+    return len(KeysView) == len(boxes)
