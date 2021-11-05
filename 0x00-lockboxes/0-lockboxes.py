913a7fd66e4efeb1041b4789890ce59d48bb2c46
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-11-05 11:24:29 -0500

    Retry

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -9,21 +9,19 @@ def canUnlockAll(boxes):
 		True if all boxes can be opened, False otherwise
 	"""
 
-	if not boxes:
-    		return False
-
-    # the first box is always open
-    	list = [0]
-    # keep track of the boxes that have been opened
-    	for box in range(len(boxes)):
-        # if the box is already opened, skip it
-        	for test_key in range(box, len(boxes)):
-            		if test_key in list:
-                		for key in boxes[test_key]:
-                    			if key not in list:
-                        			[list.append(key)]
-    # if all the boxes have been opened, return True
-    	for BoxKey in range(len(boxes)):
-        	if BoxKey not in list:
-            		return False
-    	return True
+	if type(boxes) is not list:
+		return False
+	# if there are no boxes, return True
+	if len(boxes) == 0:
+		return False
+	for key in range(1, len(boxes) - 1):
+		viewed = False
+	# if the key is not in the list of keys, return False
+		for index in range(len(boxes)):
+			viewed = key in boxes[index] and key != index
+			# print(key, index, viewed)
+			if viewed:
+				break
+			if viewed is False:
+				return viewed
+	return True
