7f3b4d0b584f31a757d9f27ded5859f50bf8f116
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-11-23 15:16:00 -0600

    Try again

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,15 +2,15 @@
 """Create a method that caluclates the fewest number of ops
 needed to result in exatly n H characters in the file"""
 
+
 def minOperations(n):
 
-	"""Return an int or zero if n is impossible to achieve
+    """Return an int or zero if n is impossible to achieve
 
 	"""
 
-	if n < 1:
-		return 0
-
-	else:
-		return n
+    if n < 1:
+        return 0
+    else:
+	    return n + minOperations(n - 1)
 
