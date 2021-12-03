21c530e83760f559311d24fa5a990c380a12f756
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-12-03 10:56:26 -0600

    Forgot to update Header

--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -8,6 +8,6 @@
 /* Prototypes */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 void sandpiles_print(int grid[3][3]);
-void sandpiles_stable(int grid[3][3]);
+int sandpiles_stable(int grid[3][3]);
 
 #endif /* SANDPILES_H */
