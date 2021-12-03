f7e2a97c9eb7a1e41a5ab59e5782cd882ff482c1
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-12-02 20:54:38 -0600

    Attempting

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -11,37 +11,56 @@
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int x, y;
-	int review;
-	int grid[3][3];
-
+	
 	for (x = 0; x < 3; x++)
 	{
 		for (y = 0; y < 3; y++)
 		{
-			grid[x][y] = grid1[x][y] + grid2[x][y];
+			grid1[x][y] = grid1[x][y] + grid2[x][y];
 		}
 	}
 
-	while (sandpiles_distribute(grid1))
+	while (1)
 	{
-		for (x = 0; x < 3; x++)
+		if (grid1[0][0] > 3 || grid1[0][1] > 3 || grid1[0][2] > 3 ||
+			grid1[1][0] > 3 || grid1[1][1] > 3 || grid1[1][2] > 3 ||
+			grid1[2][0] > 3 || grid1[2][1] > 3 || grid1[2][2] > 3)
 		{
-			for (y = 0; y < 3; y++)
+			printf("=\n");
+			sandpiles_print(grid1);
+
+			for (x = 0; x < 3; x++)
 			{
-				grid[x][y] = grid1[x][y] + grid2[x][y];
+				for (y = 0; y < 3; y++)
+				{
+					if (grid1[x][y] > 3)
+					{
+						grid1[x][y] = grid1[x][y] - 4;
+						if (x - 1 >= 0)
+							grid1[x - 1][y]++;
+						if (x + 1 < 3)
+							grid1[x + 1][y]++;
+						if (y - 1 >= 0)
+							grid1[x][y - 1]++;
+						if (y + 1 < 3)
+							grid1[x][y + 1]++;
+					}
+				}
 			}
 		}
+		else
+			break;
 	}
 }
 
 /**
- * @sandpiles_distribute - distributes the sand
+ * @sandpiles_stable - checks if a sandpile is stable
  * 
  * @grid: grid to check
  * 
  * Return: 0 if topple occured, 1 if not 
  */
-void sandpiles_distribute(int grid[3][3])
+bool sandpiles_stable(int grid[3][3])
 {
 	int x, y;
 
