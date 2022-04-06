aa03a19512b0bfaebd860ae36a65486821cd7263
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2022-04-06 12:15:01 -0500

    Code works w.o pep8, If I pep8 the code breaks.

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,13 +9,13 @@ def rotate_2d_matrix(matrix):
 	Rotate a 2D matrix 90 degrees
 	"""
 
-    """Transpose the matrix"""
-    for i in range(len(matrix)):
-        for j in range(i, len(matrix)):
-        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+	# Transpose the matrix
+	for i in range(len(matrix)):
+		for j in range(i, len(matrix)):
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    """Reverse each row"""
-    for i in range(len(matrix)):
-        matrix[i].reverse()
+	# Reverse each row
+	for i in range(len(matrix)):
+		matrix[i].reverse()
 
-    return matrix
+	return matrix
