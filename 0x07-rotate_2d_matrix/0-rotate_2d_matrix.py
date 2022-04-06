a8c0be7a0336ef97e9e697e904f3c6ba1ff13e83
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2022-04-06 12:07:22 -0500

    adjust to correct code break

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,8 +6,8 @@ Take in a 2D matrix and rotate it 90 degrees
 
 def rotate_2d_matrix(matrix):
     """
-Rotate a 2D matrix 90 degrees
-"""
+    Rotate a 2D matrix 90 degrees
+    """
 
 # Transpose the matrix
 for i in range(len(matrix)):
