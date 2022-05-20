a7b608b34261fb46b8c7c82a66c588005a0f3902
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2022-05-19 22:42:39 -0500

    Pep8

--- 0x1C-primegame/0-prime_game.py
+++ 0x1C-primegame/0-prime_game.py
@@ -34,12 +34,12 @@ def isWinner(x, nums):
 def Check_Prime(a):
     """""
 	Checking if the list is prime or not.
-	"""
+    """
     P_Num = []
     Sift = [True] * (a + 1)
     for i in range(2, a + 1):
         if Sift[i]:
             P_Num.append(i)
             for j in range(i * i, a + 1, i):
-                 Sift[j] = False
+                Sift[j] = False
     return P_Num
